d74c7f19b30c42fbd23453ecd89629aa4bafbc7f
LorcanBrophy <20112489@mail.wit.ie>
2026-02-11 14:50:33 +0000

    intial commit

---
+++ src/main/java/com/example/dsa2_ca1/controller/Controller.java
@@ -0,0 +1,8 @@
+package com.example.dsa2_ca1.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.dsa2_ca1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.dsa2_ca1 to javafx.fxml;
+    exports com.example.dsa2_ca1;
+    exports com.example.dsa2_ca1.controller;
+    opens com.example.dsa2_ca1.controller to javafx.fxml;
+    exports com.example.dsa2_ca1.main;
+    opens com.example.dsa2_ca1.main to javafx.fxml;
+}
